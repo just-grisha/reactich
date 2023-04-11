0b3c562107bbc8d52d56e60ba22249fdd6eb8d2f
Grisha <just.grishaayf@gmail.com>
2023-04-11 18:23:03 +0300

    Whatido Section added

---
+++ src/Components/Whatido/WhatidoComponents/TittleComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../WhatidosStyles/TittleStyle.css'
+    
+export default function TittleComponent() {
+  return (
+    <div className="whatido__text">
+        <div className='tlttle__text'>
+        What I Do</div>
+        </div>
+  )
+}
